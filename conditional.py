623db4bb370573f9682be2406881db1ebdd05285
AnubhavGitHub07 <prince1dwivedi00@gmail.com>
2025-10-26 21:32:37 +0530

    Conditional

---
+++ conditional.py
@@ -0,0 +1,16 @@
+age = 18
+if age >=18:
+    print("You are an adult ")
+else:
+    print("You are not an adult")
+
+marks = 85
+
+if marks >=90:
+    print("Grade : A+")
+elif marks >=75:
+    print("Grade :  A")
+elif marks >= 60:
+    print("Grade : B")
+else:
+    print("Grade : C")
